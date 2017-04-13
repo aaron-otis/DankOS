58f3556b95fd3ea31e6539886102e15681003e1a
Aaron Otis <otis@openmailbox.org>
2017-04-13 10:56:34 -0700

    Added remaining required functionality.

--- src/lib/stdio.c
+++ src/lib/stdio.c
@@ -1,4 +1,5 @@
 #include <stdarg.h>
+#include <limits.h>
 #include "stdio.h"
 #include "string.h"
 #include "types.h"
@@ -17,98 +18,121 @@
 #define DECIMAL_BASE 10
 #define HEX_BASE 16
 #define ASCII_ZERO '0'
+#define HEX_LOWER_START 'a'
+#define POS_SIGN 1
+#define NEG_SIGN -1
+
+/* Flag characters. */
+#define NO_FLAGS 0
+#define PAD_FLAG '0'
+#define LEFT_ADJ_FLAG '-'
+#define PAD_POS_FLAG ' '
+#define SIGN_FLAG '+'
 
 static unsigned int modifier;
+static unsigned char flag;
 
 void print_char(char c) {
     VGA_display_char(c);
 }
 
-int build_string(char **str, unsigned long long val, int base) {
-    int len, i;
+int build_string(char **str, unsigned long long val, int base, int sign) {
+    int len, i, size;
 
     /* Determine the string length. */
     if (modifier == LONG_ARG)
-        len = LONG_STR_LEN;
+        size = len = LONG_STR_LEN;
     else if (modifier == SHORT_ARG)
-        len = SHORT_STR_LEN;
+        size = len = SHORT_STR_LEN;
     else if (modifier == QUAD_ARG)
-        len = QUAD_STR_LEN;
+        size = len = QUAD_STR_LEN;
     else
-        len = INT_STR_LEN;
+        size = len = INT_STR_LEN;
+
+    if (sign == NEG_SIGN)
+        val *= -1;
 
     /* Get individual digits. */
     str[len] = 0;
     for (; val > 0 && len; val /= base) {
-        (*str)[--len] = (val % base) + ASCII_ZERO;
+        (*str)[--len] = (val % base);
 
         if (len < 0)
             return STDIO_INVALID_LENGTH;
+
+        /* Convert value to the correct character. */
+        if ((*str)[len] >= DECIMAL_BASE)
+            (*str)[len] += HEX_LOWER_START - DECIMAL_BASE;
+        else
+            (*str)[len] += ASCII_ZERO;
+
     }
 
     /* Adjust string for printing. */
     if (len > 0) {
-        for (i = 0; i < len; i++)
+        for (i = 0; i < size; i++)
             (*str)[i] = (*str)[i + len];
 
-        (*str)[len] = 0;
+        (*str)[size - len] = 0;
     }
 
     return EXIT_SUCCESS;
 }
 
 void print_int(int i) {
-    int len;
+    int sign;
     char buf[LONG_STR_LEN + 1], *str;
 
     str = buf;
+    if (i < 0) { /* Check if integer is negative. */
+        *str = '-';
+    }
+    else
+        *str = '+';
     str++;
-    build_string(&str, i, DECIMAL_BASE);
+    sign = i < 0 ? NEG_SIGN : POS_SIGN;
+
+    if (0 > build_string(&str, i, DECIMAL_BASE, sign))
+        VGA_display_str("\nprint_int error\n");
 
     if (i < 0)
-        *--str = '-';
+        str--;
 
     VGA_display_str(str);
 }
 
 void print_uint(unsigned int u) {
-    int len, i;
     char str[LONG_STR_LEN];
     char *buf;
 
-    /* Determine the string length. */
-    /*
-    if (modifier == LONG_ARG)
-        len = LONG_STR_LEN;
-    else if (modifier == SHORT_ARG)
-        len = SHORT_STR_LEN;
-    else if (modifier == QUAD_ARG)
-        len = QUAD_STR_LEN;
-    else
-        len = INT_STR_LEN;
-
-    str[len] = 0;
-    for (; u > 0 && len; u /= DECIMAL_BASE) {
-        str[--len] = (u % DECIMAL_BASE) + ASCII_ZERO;
-    }
-
-    if (len > 0) {
-        for (i = 0; i < len; i++)
-            str[i] = str[i + len];
-
-        str[len] = 0;
-    }
-    */
     buf = str;
-    build_string(&buf, u, DECIMAL_BASE);
+    if (0 > build_string(&buf, u, DECIMAL_BASE, POS_SIGN))
+        VGA_display_str("\nprint_uint error\n");
 
     VGA_display_str(str);
 }
 
 void print_hex(unsigned int u) {
+    char str[LONG_STR_LEN], *buf;
+
+    buf = str;
+    if (0 > build_string(&buf, u, HEX_BASE, POS_SIGN))
+        VGA_display_str("\nprint_uint error\n");
+
+    VGA_display_str(str);
 }
 
 void print_ptr(void *p) {
+    char str[LONG_STR_LEN + 2], *buf;
+
+    str[0] = '0';
+    str[1] = 'x';
+    buf = str + 2;
+
+    if (0 > build_string(&buf, (unsigned long) p, HEX_BASE, POS_SIGN))
+        VGA_display_str("\nprint_ptr error\n");
+
+    VGA_display_str(str);
 }
 
 void print_str(char *str) {
@@ -154,15 +178,36 @@ __attribute__ ((format (printf, 1, 2))) extern int printk(const char *fmt,
                     break;
                 
                 case INT_DELIM:
-                    print_int(va_arg(ap, int));
+                    if (modifier == SHORT_ARG)
+                        print_int(va_arg(ap, int));
+                    else if (modifier == LONG_ARG)
+                        print_int(va_arg(ap, long int));
+                    else if (modifier == QUAD_ARG)
+                        print_int(va_arg(ap, long long int));
+                    else
+                        print_int(va_arg(ap, int));
                     break;
 
                 case UINT_DELIM:
-                    print_uint(va_arg(ap, unsigned int));
+                    if (modifier == SHORT_ARG)
+                        print_uint(va_arg(ap, int));
+                    else if (modifier == LONG_ARG)
+                        print_uint(va_arg(ap, long int));
+                    else if (modifier == QUAD_ARG)
+                        print_uint(va_arg(ap, long long int));
+                    else
+                        print_uint(va_arg(ap, unsigned int));
                     break;
 
                 case HEX_DELIM:
-                    print_hex(va_arg(ap, unsigned int));
+                    if (modifier == SHORT_ARG)
+                        print_hex(va_arg(ap, unsigned int));
+                    else if (modifier == LONG_ARG)
+                        print_hex(va_arg(ap, unsigned long int));
+                    else if (modifier == QUAD_ARG)
+                        print_hex(va_arg(ap, unsigned long long int));
+                    else
+                        print_hex(va_arg(ap, unsigned int));
                     break;
 
                 case CHAR_DELIM:
