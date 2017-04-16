df486341dc1a09d07e8fe9e385f3be65da84d9fb
Aaron Otis <otis@openmailbox.org>
2017-04-15 23:29:45 -0700

    Moved initialization code to init.c.

--- src/kernel.c
+++ src/kernel.c
@@ -1,9 +1,7 @@
 #include "kernel.h"
 #include "kernel_tests.h"
 #include "lib/debug.h"
-#include "drivers/vga.h"
-#include "drivers/ps2.h"
-#include "lib/stdio.h"
+#include "init.h"
 
 static void halt_cpu() {
 
@@ -17,33 +15,16 @@ int kernel_main() {
      * Initializations.
      */
 
-    /* Initialize VGA driver. */
-    if (VGA_init() == EXIT_FAILURE)
+    if (init() == EXIT_FAILURE)
         halt_cpu();
-
-    /* Set VGA attributes so that printing from the kernel can be seen. */
-    VGA_set_attr(VGA_WHITE, VGA_BLACK, 0);
-
-    /* Initialize PS2 driver. */
-    if (PS2_init() == EXIT_FAILURE) {
-        printk("PS2 driver initialization failure\n");
-        halt_cpu();
-    }
 
     /* 
      * Run kernel tests. 
      */
 
-    /* VGA driver tests. */
-    vga_driver_tests();
+    //run_all_tests();
 
-    /* stdio tests. */
-    stdio_tests();
-
-    /* PS2 tests. */
-    ps2_tests();
-
-    /* Halt cpu at end for testing. */
+    /* Halt CPU at end for testing. */
     halt_cpu();
 
     return 0;
