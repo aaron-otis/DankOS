8416c8120cfac4fe520c028f0b21adc1bb784e5b
Aaron Otis <otis@openmailbox.org>
2017-04-19 15:20:17 -0700

    Fixed bug that corrupted the stack when printing shorts. Added backspace functionality. Disabled code that returns the length of the string printed.

--- src/lib/stdio.c
+++ src/lib/stdio.c
@@ -19,8 +19,10 @@
 #define HEX_BASE 16
 #define ASCII_ZERO '0'
 #define HEX_LOWER_START 'a'
+
 #define POS_SIGN 1
 #define NEG_SIGN -1
+#define SHORT_MASK 0xFFFF
 
 /* Flag characters. */
 #define NO_FLAGS 0
@@ -29,43 +31,57 @@
 #define PAD_POS_FLAG ' '
 #define SIGN_FLAG '+'
 
+#define BACKSPACE '\b'
+
 static unsigned int modifier;
 static unsigned char flag;
 
 void print_char(char c) {
-    VGA_display_char(c);
+
+    if (c == BACKSPACE)
+        VGA_backspace();
+    else
+        VGA_display_char(c);
 }
 
 static int build_string(char **str, unsigned long long val, int base, int sign) {
-    int len, i, size;
+    int len, i, size, debug = 1;
 
     /* Determine the string length. */
-    if (modifier == LONG_ARG)
-        size = len = LONG_STR_LEN;
+    if (modifier == LONG_ARG) {
+        len = LONG_STR_LEN;
+        //while(debug);
+    }
     else if (modifier == SHORT_ARG)
-        size = len = SHORT_STR_LEN;
+        len = SHORT_STR_LEN;
     else if (modifier == QUAD_ARG)
-        size = len = QUAD_STR_LEN;
+        len = QUAD_STR_LEN;
     else
-        size = len = INT_STR_LEN;
+        len = INT_STR_LEN;
+
+    size = len;
 
     if (sign == NEG_SIGN)
-        val *= -1;
+        val = ~val + 1;
 
     /* Get individual digits. */
-    str[len] = 0;
-    for (; val > 0 && len; val /= base) {
-        (*str)[--len] = (val % base);
+    (*str)[len] = 0;
+    if (val == 0)
+        (*str)[--len] = '0';
+    else {
+        for (; val > 0 && len; val /= base) {
+            (*str)[--len] = (val % base);
 
-        if (len < 0)
-            return STDIO_INVALID_LENGTH;
+            if (len < 0)
+                return STDIO_INVALID_LENGTH;
 
-        /* Convert value to the correct character. */
-        if ((*str)[len] >= DECIMAL_BASE)
-            (*str)[len] += HEX_LOWER_START - DECIMAL_BASE;
-        else
-            (*str)[len] += ASCII_ZERO;
+            /* Convert value to the correct character. */
+            if ((*str)[len] >= DECIMAL_BASE)
+                (*str)[len] += HEX_LOWER_START - DECIMAL_BASE;
+            else
+                (*str)[len] += ASCII_ZERO;
 
+        }
     }
 
     /* Adjust string for printing. */
@@ -105,13 +121,15 @@ static int print_int(int i) {
 static int print_uint(unsigned int u) {
     char str[LONG_STR_LEN];
     char *buf;
+    int len;
 
     buf = str;
-    if (0 > build_string(&buf, u, DECIMAL_BASE, POS_SIGN))
+    len = build_string(&buf, u, DECIMAL_BASE, POS_SIGN);
+    if (0 > len)
         VGA_display_str("\nprint_uint error\n");
 
     VGA_display_str(str);
-    return strlen(str);
+    return len;
 }
 
 static int print_hex(unsigned int u) {
@@ -145,9 +163,8 @@ static int print_str(char *str) {
 }
 
 
-__attribute__ ((format (printf, 1, 2))) extern int printk(const char *fmt, 
- ...) {
-    int i, len = 0;
+extern int printk(const char *fmt, ...) {
+    int i, len = 0, debug = 1;
     va_list ap;
 
     va_start(ap, fmt);
@@ -174,59 +191,59 @@ __attribute__ ((format (printf, 1, 2))) extern int printk(const char *fmt,
                     break;
                 default:
                     modifier = NORMAL_ARG;
+                    break;
             }
 
             /* Check for conversion specifiers. */
             switch (fmt[i]) {
                 case FMT_DELIM:
                     print_char(fmt[i]);
-                    len++;
                     break;
                 
                 case INT_DELIM:
                     if (modifier == SHORT_ARG)
-                        len += print_int(va_arg(ap, int));
+                        print_int(va_arg(ap, int));
                     else if (modifier == LONG_ARG)
-                        len += print_int(va_arg(ap, long int));
+                        print_int(va_arg(ap, long int));
                     else if (modifier == QUAD_ARG)
-                        len += print_int(va_arg(ap, long long int));
+                        print_int(va_arg(ap, long long int));
                     else
-                        len += print_int(va_arg(ap, int));
+                        print_int(va_arg(ap, int));
                     break;
 
                 case UINT_DELIM:
                     if (modifier == SHORT_ARG)
-                        len += print_uint(va_arg(ap, int));
+                        print_uint(va_arg(ap, unsigned int));
                     else if (modifier == LONG_ARG)
-                        len += print_uint(va_arg(ap, long int));
+                        print_uint(va_arg(ap, unsigned long int));
                     else if (modifier == QUAD_ARG)
-                        len += print_uint(va_arg(ap, long long int));
+                        print_uint(va_arg(ap, unsigned long long int));
                     else
-                        len += print_uint(va_arg(ap, unsigned int));
+                        print_uint(va_arg(ap, unsigned int));
+
                     break;
 
                 case HEX_DELIM:
                     if (modifier == SHORT_ARG)
-                        len += print_hex(va_arg(ap, unsigned int));
+                        print_hex(va_arg(ap, unsigned int));
                     else if (modifier == LONG_ARG)
-                        len += print_hex(va_arg(ap, unsigned long int));
+                        print_hex(va_arg(ap, unsigned long int));
                     else if (modifier == QUAD_ARG)
-                        len += print_hex(va_arg(ap, unsigned long long int));
+                        print_hex(va_arg(ap, unsigned long long int));
                     else
-                        len += print_hex(va_arg(ap, unsigned int));
+                        print_hex(va_arg(ap, unsigned int));
                     break;
 
                 case CHAR_DELIM:
                     print_char(va_arg(ap, int));
-                    len++;
                     break;
 
                 case PTR_DELIM:
-                    len += print_ptr(va_arg(ap, void *));
+                    print_ptr(va_arg(ap, void *));
                     break;
 
                 case STR_DELIM:
-                    len += print_str(va_arg(ap, char *));
+                    print_str(va_arg(ap, char *));
                     break;
 
                 default:
@@ -235,7 +252,6 @@ __attribute__ ((format (printf, 1, 2))) extern int printk(const char *fmt,
         }
         else {
             print_char(fmt[i]);
-            len++;
         }
     }
 
