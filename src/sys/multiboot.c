d2a5c4cbaa4396cd8c830cfc0c97682e652e7d27
Aaron Otis <otis@openmailbox.org>
2017-05-06 13:07:38 -0700

    Added parsing of Multiboot 2 tags. Currently tracked tags are for memory and elf sections.

---
+++ src/sys/multiboot.c
@@ -0,0 +1,100 @@
+#include "multiboot.h"
+#include "../lib/stdio.h"
+#include "../lib/stdlib.h"
+#include <limits.h>
+
+#define HIGH_ADDR_START 0x9fc00
+
+MB_mem_info MB_parse_tags(MB_basic_tag *mb_tag) {
+    int i, tag_size;
+    uint8_t *ptr = (uint8_t *) mb_tag;
+    uint8_t *mb_tag_end, *mmap_tag_end, *elf_tag_end;
+    MB_fixed_tag_header *fixed_header;
+    MB_mmap_tag *mmap;
+    MB_ELF_symb_tag *elf;
+    MB_mmap_entry *mmap_entry;
+    MB_ELF_section_header *elf_section;
+    MB_mem_info mem_info;
+
+    mb_tag_end = (uint8_t *) (ptr + mb_tag->size);
+    printk("Total size: %u\n", mb_tag->size);
+    ptr += sizeof(MB_basic_tag);
+
+    /* Parse all multiboot 2 tags. */
+    printk("Tags found:\n");
+    while (ptr < mb_tag_end) {
+        fixed_header = (MB_fixed_tag_header *) ptr;
+        printk("Tag type: %u\n", fixed_header->type); /* Debugging info. */
+        printk("Tag size: %u\n\n", fixed_header->size); /* Debugging info. */
+
+        tag_size = fixed_header->size; /* Determine tag size. */
+        if (tag_size % CHAR_BIT) /* Ensure size is 8 byte aligned. */
+            tag_size += CHAR_BIT - (tag_size % CHAR_BIT);
+
+        /* Check for relevant tags (mmap and elf). */
+        if (fixed_header->type == MULTI_ELF) {
+            elf = (MB_ELF_symb_tag *) (ptr + sizeof(MB_basic_tag));
+            elf_tag_end = ptr + tag_size;
+        }
+        else if (fixed_header->type == MULTI_MMAP) {
+            mmap = (MB_mmap_tag *) (ptr + sizeof(MB_basic_tag));
+            mmap_tag_end = ptr + tag_size;
+        }
+
+        ptr += tag_size; /* Advance to the next tag. */
+    }
+
+    printk("ELF stats:\n");
+    printk("num: %u\nentsize: %u\nshndx %u\n\n", elf->num, elf->entsize, 
+     elf->shndx);
+
+    /* Set up pointer to first ELF section header. */
+    ptr = (uint8_t *) elf;
+    ptr += sizeof(MB_ELF_symb_tag);
+    elf_section = (MB_ELF_section_header *) ptr;
+
+    for (i = 0; elf_section < (MB_ELF_section_header *) elf_tag_end && 
+     i < elf->num; elf_section++, i++) {
+        printk("name: 0x%x\ntype: 0x%x\n", elf_section->name, elf_section->type);
+        printk("flags: 0x%qx\naddress: %qp\noffset: %qu\n", elf_section->flags, 
+         elf_section->address, elf_section->offset);
+        printk("size: %qu\nlink: %u\nextra: %u\n", elf_section->size, 
+         elf_section->table_index_link, elf_section->extra_info);
+        printk("alignment: %qu\nIFF: %qu\n",elf_section->addr_alignment, 
+         elf_section->IFF);
+        printk("end address: %qp\n\n", elf_section->address + elf_section->size);
+
+        if (i == 1)
+            mem_info.kern_start = elf_section->address;
+        if (i == elf->num - 1)
+            mem_info.kern_size = (elf_section->address + elf_section->size) - 
+             mem_info.kern_start;
+    }
+
+    /* Set up pointer to first mmap entry. */
+    ptr = (uint8_t *) mmap;
+    ptr += sizeof(MB_mmap_tag);
+    mmap_entry = (MB_mmap_entry *) ptr;
+
+    printk("mmap stats:\n");
+    printk("size: %u\nversion %u\n", mmap->entry_size, mmap->entry_version);
+    printk("Memory entries found: \n\n");
+
+    for (i = 0;mmap_entry <  (MB_mmap_entry *) mmap_tag_end; mmap_entry++) {
+        printk("base addr: %qp\nlength: %qu\ntype: %u\n\n", 
+         mmap_entry->base_addr, mmap_entry->length, mmap_entry->type);
+
+        if (mmap_entry->type == MULTI_MEM_USABLE) {
+            if (!i) {
+                mem_info.low.address = mmap_entry->base_addr;
+                mem_info.low.size = mmap_entry->length;
+            }
+            else if (i == 1) {
+                mem_info.high.address = mmap_entry->base_addr;
+                mem_info.high.size = mmap_entry->length;
+            }
+        }
+    }
+
+    return mem_info;
+}
---
+++ src/sys/multiboot.h
@@ -0,0 +1,97 @@
+#ifndef _MULITIBOOT_H
+#define _MULITIBOOT_H
+
+#include "../lib/stdint.h"
+#include "memory.h"
+
+#define MULTI_MAGIC 0xE85250D6
+#define MULTI_ARCH 0
+#define MULTI_MMAP 6
+#define MULTI_ELF 9
+#define MULTI_MEM_USABLE 1
+
+typedef struct {
+    uint32_t magic;
+    uint32_t arch;
+    uint32_t length; /* Length of of entire header. */
+    uint32_t checksum; /* Results in 0 when added to other fields. */
+    void *tags;
+} __attribute__((packed)) MB_header;
+
+typedef struct {
+    uint16_t type;
+    uint16_t flags;
+    uint32_t size;
+} __attribute__((packed)) MB_basic_header_tag;
+
+typedef struct {
+    uint32_t size; /* Total size (bytes) including terminating tag. */
+    uint32_t reserved;
+} __attribute__((packed)) MB_basic_tag;
+
+typedef struct {
+    uint32_t type;
+    uint32_t size;
+} __attribute__((packed)) MB_fixed_tag_header;
+
+typedef struct {
+    uint32_t upper;
+    uint32_t lower;
+} __attribute__((packed)) MB_mem_info_tag;
+
+typedef struct {
+    uint32_t biosdev;
+    uint32_t partition;
+    uint32_t subparition;
+} __attribute__((packed)) MB_boot_dev_tab;
+
+typedef struct {
+    uint8_t string;
+} __attribute__((packed)) MB_cmd_tag;
+
+typedef struct {
+    uint32_t num;
+    uint32_t entsize;
+    uint32_t shndx;
+} __attribute__((packed)) MB_ELF_symb_tag;
+
+typedef struct {
+    uint32_t name;
+    uint32_t type;
+    uint64_t flags;
+    uint64_t address;
+    uint64_t offset;
+    uint64_t size; /* In bytes. */
+    uint32_t table_index_link;
+    uint32_t extra_info;
+    uint64_t addr_alignment; /* In powers of 2. */
+    uint64_t IFF;
+} __attribute__((packed)) MB_ELF_section_header;
+
+typedef struct {
+    uint64_t base_addr;
+    uint64_t length;
+    uint32_t type;
+    uint32_t reserved;
+} __attribute__((packed)) MB_mmap_entry;
+
+typedef struct {
+    uint32_t entry_size;
+    uint32_t entry_version;
+} __attribute__((packed)) MB_mmap_tag;
+
+typedef struct {
+    uint64_t address;
+    uint64_t size;
+} MB_mem_block;
+
+typedef struct {
+    MB_mem_block low;
+    MB_mem_block high;
+    long kern_start;
+    int kern_size;
+} MB_mem_info;
+
+MB_mem_info MB_parse_tags(MB_basic_tag *);
+
+#endif
