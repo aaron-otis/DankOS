66777d04577a4b5b7117294f62eafe8161035acf
Aaron Otis <otis@openmailbox.org>
2017-05-14 15:16:24 -0700

    Added structures necessary to implement a page table.

--- src/sys/memory.h
+++ src/sys/memory.h
@@ -3,7 +3,22 @@
 
 #include "../lib/stdint.h"
 
+/* Virtual Address Space Layout:
+ *
+ * Identity map         0x0
+ * Kernel stacks        0x10000000000
+ * Reserved/Growth      0x20000000000
+ * Kernel heap          0xF0000000000
+ * User space           0x100000000000
+ */
+
 #define PAGE_SIZE 4096
+#define KSTACKS_ADDR 0x10000000000
+#define KRESERVED_ADDR 0x20000000000
+#define KHEAP_ADDR 0xF0000000000
+
+#define USR_SPC_STACK_SIZE
+#define USR_SPC_HEAP_SIZE
 
 typedef struct {
     void *address;
@@ -13,12 +28,82 @@ typedef struct {
 /** @brief Node for the free list of page frames.
  */
 typedef struct page_frame {
-    void *address;
     struct page_frame *next;
 } page_frame;
 
+/** @brief Structure of CR3 Register.
+ *
+ * Bit 3 is the Page-Level Writethrough: 0 denotes a writeback caching policy, 
+ * while 1 denote a writethrough caching policy.
+ * Bit 4 is the Page-Level Cache Disabled: 0 denotes cacheable, 1 denotes not 
+ * cacheable.
+ * In long mode, a 40-bit base address is used.
+ */
+typedef struct cr3_register {
+    uint64_t reserved1:3;
+    uint64_t pwt:1;
+    uint64_t pcd:1;
+    uint64_t reserved2:7;
+    uint64_t base_addr:40;
+    uint64_t reserved3:12;
+} __attribute__((packed)) CR3;
+
+typedef struct page_map_level_4 {
+    uint64_t present:1;
+    uint64_t r_w:1; /* Read/write bit. */
+    uint64_t u_s:1; /* User/supervisor mode. Set to 1 for user mode. */
+    uint64_t pwt:1; /* Page-Level Writethrough. */
+    uint64_t pcd:1; /* Page Caching Disabled. */
+    uint64_t a:1;   /* Accessed bit. */
+    uint64_t ign:1;
+    uint64_t mbz:2;
+    uint64_t avl:3; /* Available to software. */
+    uint64_t base_addr:51; /* 40 bit base address combined with 11 bit avail. */
+    uint64_t nx:1;
+} __attribute__((packed)) PML4;
+
+typedef struct page_directory_pointer_table {
+    uint64_t present:1;
+    uint64_t r_w:1;
+    uint64_t u_s:1;
+    uint64_t pwt:1;
+    uint64_t pcd:1;
+    uint64_t a:1;
+    uint64_t ign:1;
+    uint64_t zero:1;
+    uint64_t mbz:1;
+    uint64_t avl:3;
+    uint64_t base_addr:51;
+    uint64_t nx:1;
+} __attribute__((packed)) PDP;
+
+typedef struct page_directory_table PD;
+
+typedef struct page_table {
+    uint64_t present:1;
+    uint64_t r_w:1;
+    uint64_t u_s:1;
+    uint64_t pwt:1;
+    uint64_t pcd:1;
+    uint64_t a:1;
+    uint64_t d:1;           /* Dirty bit. */
+    uint64_t pat:1;         /* Page-Attribute Table. */
+    uint64_t global_page:1;
+    uint64_t avl:3;
+    uint64_t base_addr:51;
+    uint64_t nx:1;
+} __attribute__((packed)) PT;
+
+/* Page frame allocator. */
 void MMU_pf_init();
 void *MMU_pf_alloc(void);
 int MMU_pf_free(void *pf);
+
+/* Virtual page allocator. */
+int MMU_init();
+void *MMU_alloc_page();
+void *MMU_alloc_pages(int num);
+void MMU_free_page(void *);
+void MMU_free_pages(void *, int num);
 
 #endif
