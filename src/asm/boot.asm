559e1fae1ef87523f711dfa32303048d34b0c2e7
Aaron Otis <otis@openmailbox.org>
2017-06-13 19:50:27 -0700

    Added SEE support.

--- src/asm/boot.asm
+++ src/asm/boot.asm
@@ -15,6 +15,15 @@ start:
     call set_up_page_tables         ; Setup and enable page tables.
     call enable_paging
 
+    ; Enable SSE support.
+    mov eax, cr0
+    and ax, 0xFFFB      ;clear coprocessor emulation CR0.EM
+    or ax, 0x2          ;set coprocessor monitoring  CR0.MP
+    mov cr0, eax
+    mov eax, cr4
+    or ax, 3 << 9       ;set CR4.OSFXSR and CR4.OSXMMEXCPT at the same time
+    mov cr4, eax
+
     lgdt [gdt64.pointer]            ; Load the 64-bit GDT.
     jmp gdt64.code:long_mode_start
 
