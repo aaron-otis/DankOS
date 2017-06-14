32461a1ce4a16f7841083159bf4797eef66f8ab9
Aaron Otis <otis@openmailbox.org>
2017-06-13 21:36:12 -0700

    Implemented cooperative multitasking.

--- src/sys/proc.c
+++ src/sys/proc.c
@@ -1,18 +1,176 @@
 #include "proc.h"
+#include "syscalls.h"
+#include "kmalloc.h"
+#include "memory.h"
+#include "../drivers/interrupts.h"
+#include "../lib/string.h"
+#include "../lib/stdlib.h"
+#include "../gdt.h"
+#include <stddef.h>
 
+#define RFLAG_INT_ENABELED (1 << 9)
 
-proc_t cur_proc;
-proc_t next_proc;
+static proc_t *main_proc_ptr;
+static proc_t *process_list, *blocked_list;
+static proc_t *process_list_tail, *blocked_list_tail;
+static unsigned long long next_pid;
+proc_t *cur_proc;
+proc_t *next_proc;
 
-void PROC_run(void) {
+static int schedule_proc(proc_t *proc) {
+    proc_t *tail;
+    int ret = EXIT_SUCCESS, ints_enabled = 0;
+
+    if (are_interrupts_enabled()) {
+        ints_enabled = 1;
+        CLI;
+    }
+
+    tail = process_list_tail;
+    tail->next = proc;
+    process_list_tail = proc;
+
+    if (!process_list)
+        process_list = process_list_tail;
+
+    if (ints_enabled)
+        STI;
+
+    return ret;
 }
 
-void PROC_create_kthread(kproc_t entry_point, void *arg) {
+static int block_proc(proc_t *proc) {
+
+    return EXIT_SUCCESS;
 }
 
 void PROC_reschedule(void) {
+    int ints_enabled = 0;
+
+    if (are_interrupts_enabled()) {
+        ints_enabled = 1;
+        CLI;
+    }
+
+    if (process_list) {
+        next_proc = process_list;
+        process_list = process_list->next;
+    }
+    else {
+        next_proc = main_proc_ptr;
+        next_proc->rflags = RFLAG_INT_ENABELED;
+    }
+
+    if (ints_enabled)
+        STI;
 }
 
-void kexit(void) {
+void PROC_yield_isr(int irq, int err, void *arg) {
+
+    PROC_reschedule();
 }
 
+void PROC_exit_isr(int irq, int err, void *arg) {
+    proc_t *head = process_list, *prev;
+
+    while (cur_proc && head != cur_proc) {
+        prev = head;
+        head = head->next;
+    }
+
+    if (cur_proc) /* Remove |cur_proc| from |process_list|. */
+        prev->next = head->next;
+
+    PROC_reschedule();
+    MMU_free_kstack((void *) head->rsp);
+    free(head);
+    cur_proc = NULL;
+}
+
+void yield_syscall(void *arg) {
+    PROC_reschedule();
+}
+
+void yield(void) {
+    SYSCALL_generic_syscall(0, 0, 0, YIELD_SYSCALL);
+}
+
+void PROC_init(void) {
+    int ints_enabled = 0;
+
+    if (are_interrupts_enabled()) {
+        ints_enabled = 1;
+        CLI;
+    }
+
+    cur_proc = next_proc = NULL; /* No processes scheduled yet. */
+    main_proc_ptr = NULL; /* Main process struct not created yet. */
+    process_list = blocked_list = NULL; /* No threads created yet. */
+    next_pid = 1; /* First pid. */
+
+    /* Register yield system call. */
+    SYSCALL_register_syscall(YIELD_SYSCALL, yield_syscall, NULL);
+
+    /* Register ISR handler for yield and exit functions. */
+    IRQ_set_handler(PROC_YIELD_INT, PROC_yield_isr, NULL);
+    IRQ_set_handler(PROC_EXIT_INT, PROC_exit_isr, NULL);
+
+    if (ints_enabled)
+        STI;
+}
+
+void PROC_run(void) {
+    proc_t main_proc;
+    int ints_enabled = 0;
+
+    if (are_interrupts_enabled()) {
+        ints_enabled = 1;
+        CLI;
+    }
+
+    memset(&main_proc, 0, sizeof(main_proc)); /* Zero |main_proc|. */
+    main_proc_ptr = &main_proc;
+    main_proc.cs = KERN_CS_OFFSET;
+    cur_proc = &main_proc;
+
+    if (ints_enabled)
+        STI;
+
+    yield();
+}
+
+void exit(void) {
+    __asm__ (PROC_EXIT_INT_ASM);
+}
+
+void PROC_create_kthread(kproc_t entry_point, void *arg) {
+    proc_t *proc = kmalloc(sizeof(proc_t));
+    proc_t *new;
+    int ints_enabled = 0;
+
+    if (are_interrupts_enabled()) {
+        ints_enabled = 1;
+        CLI;
+    }
+
+    memset(proc, 0, sizeof(proc_t)); /* Zero all fields. */
+    proc->rip = (uint64_t) entry_point;
+    proc->cs = KERN_CS_OFFSET;
+    proc->rsp = MMU_alloc_kstack();
+    proc->ss = 0;
+    proc->rflags = RFLAG_INT_ENABELED;
+    proc->rdi = (uint64_t) arg;
+
+    /* 
+     * Put call to |exit| at bottom of stack so that the process will exit by 
+     * default.
+     */
+    *proc->rsp = (uint64_t) exit;
+    proc->rsp--; /* Decrement stack pointer. */
+
+    /* Add to process queue. */
+    schedule_proc(proc);
+
+    if (ints_enabled)
+        STI;
+}
--- src/sys/proc.h
+++ src/sys/proc.h
@@ -1,18 +1,49 @@
 #ifndef _PROC_H
 #define _PROC_H
 
-typedef void (*kproc_t)(void*);
+#include "../lib/stdint.h"
 
+typedef void (*kproc_t)(void*);
+typedef uint64_t pid_t;
+
+void PROC_init(void);
 void PROC_run(void);
 void PROC_create_kthread(kproc_t entry_point, void *arg);
 void PROC_reschedule(void);
-void kexit(void);
-
-static inline void yield(void) {
-          asm volatile ( "INT $123" );
-}
+void exit(void);
+void yield(void);
 
 typedef struct proc_t {
-} proc_t;
+    uint64_t rax;
+    uint64_t rbx;
+    uint64_t rcx;
+    uint64_t rdx;
+    uint64_t rdi;
+    uint64_t rsi;
+    uint64_t r8;
+    uint64_t r9;
+    uint64_t r10;
+    uint64_t r11;
+    uint64_t r12;
+    uint64_t r13;
+    uint64_t r14;
+    uint64_t r15;
+    uint64_t rbp;
+    uint64_t *rsp;
+    uint64_t rip;
+    uint64_t rflags;
+    uint64_t cs;
+    uint64_t ss;
+    uint64_t ds;
+    uint64_t es;
+    uint64_t fs;
+    uint64_t gs;
+    pid_t id;
+    /* file descriptors. */
+    struct proc_t *next;
+} __attribute__ ((packed)) proc_t;
+
+extern proc_t *cur_proc;
+extern proc_t *next_proc;
 
 #endif
